b23756761c42948195d6333fecaf191ba8dcacb7
Marcial Carrillo <marcialcarrillov@gmail.com>
2018-11-21 17:36:08 -0600

    cleaning up

--- MPI_Template/MergesortMPI.cpp
+++ MPI_Template/MergesortMPI.cpp
@@ -47,9 +47,6 @@ int main(int argc, char* argv[])
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	local_start = MPI_Wtime();
-	/* Código a evaluar */
-
-	//local_start = MPI_Wtime();
 
 	get_args(argv, input_number);
 
@@ -73,11 +70,6 @@ int main(int argc, char* argv[])
 	vector_local_randoms_result.resize(range);
 	generate(begin(vector_randoms), end(vector_randoms), gen);
 
-	/*for (auto i : vec)
-	{
-	cout << i << " ";
-	}*/
-
 
 	array_randoms = vector_randoms.data();
 	array_local_randoms = vector_local_randoms.data();
@@ -97,15 +89,8 @@ int main(int argc, char* argv[])
 	{
 		int displacement = (int)pow(2, i);
 		if (process_id % (displacement * 2) == displacement)
-		{
-			 
-			//MPI_Send(array_local_randoms, range * displacement, MPI_INT, process_id - displacement, 0, MPI_COMM_WORLD);
-			MPI_Send(vector_local_randoms.data(), range * displacement, MPI_INT, process_id - displacement, 0, MPI_COMM_WORLD);
-
-
-			//vector<int> temp(public_nested_vectors[process_id].size() + public_nested_vectors[process_id + displacement].size());
-			//merge(public_nested_vectors[process_id].begin(), public_nested_vectors[process_id].end(), public_nested_vectors[process_id + displacement].begin(), public_nested_vectors[process_id + displacement].end(), temp.begin());
-			//public_nested_vectors[process_id].swap(temp);
+		{			
+			MPI_Send(vector_local_randoms.data(), range * displacement, MPI_INT, process_id - displacement, 0, MPI_COMM_WORLD);					   
 		}
 		MPI_Barrier(MPI_COMM_WORLD);
 		if (process_id % (displacement * 2) == 0)
@@ -116,22 +101,15 @@ int main(int argc, char* argv[])
 				vector_local_randoms_input.resize(range * displacement);				
 			}
 
-			//MPI_Recv(array_local_randoms_input, range * displacement, MPI_INT, process_id + displacement, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			
 			MPI_Recv(vector_local_randoms_input.data(), range * displacement, MPI_INT, process_id + displacement, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 			vector_local_randoms_result.clear();
 			vector_local_randoms_result.resize(vector_local_randoms.size() + vector_local_randoms_input.size());
-
-			//merge(vector_local_randoms.begin(), vector_local_randoms.end(), vector_local_randoms_input.begin(), vector_local_randoms_input.end(), vector_local_randoms_result.begin());
-
+			
 			merge(vector_local_randoms.begin(), vector_local_randoms.end(), vector_local_randoms_input.begin(), vector_local_randoms_input.end(), vector_local_randoms_result.begin());
 			vector_local_randoms.swap(vector_local_randoms_result);
 
-			//receive and merge
-
-			//vector<int> temp(public_nested_vectors[process_id].size() + public_nested_vectors[process_id + displacement].size());
-			//merge(public_nested_vectors[process_id].begin(), public_nested_vectors[process_id].end(), public_nested_vectors[process_id + displacement].begin(), public_nested_vectors[process_id + displacement].end(), temp.begin());
-			//public_nested_vectors[process_id].swap(temp);
 		}
 
 	}
@@ -142,42 +120,14 @@ int main(int argc, char* argv[])
 
 
 
-	//MPI_Gather(array_local_randoms, range, MPI_INT, array_randoms, range, MPI_INT, 0, MPI_COMM_WORLD);
-
-	//MPI_Barrier(MPI_COMM_WORLD);
-
-	//if (process_id == 0)
-	//{
-	//	//cout << "Prosess " << process_id << endl;
-	//	print1(vector_randoms);
-	//	//cout << vector_randoms.size() << endl;
-	//	for (int i = 1; i < process_quantity; i++)
-	//	{
-	//		auto it_middle = vector_randoms.begin() + (i * range);
-	//		auto it_end = vector_randoms.begin() + ((i + 1) * range);
-	//		inplace_merge(vector_randoms.begin(), it_middle, it_end);
-	//	}
-	//	cout << endl << endl << "COMPLETE VECTOR: " << endl << endl;
-	//	print2(vector_randoms);
-	//	cout << endl << endl << "Total Time Elapsed: " << elapsed << endl;
-	//	
-	//}
 	MPI_Barrier(MPI_COMM_WORLD);
 	if (process_id == 0)
 	{
-
-		cout << endl << endl << "COMPLETE VECTOR: " << endl << endl;
-		//vector_randoms.swap(vector_local_randoms);
-		//print2(vector_randoms);
 		print2(vector_local_randoms);
 		cout << endl << endl << "Total Time Elapsed: " << elapsed << endl;
-		cout << "Vector size5: " << vector_local_randoms.size();
-
+		cin.ignore();
 	}
 
-	if (process_id == 0)
-		cin.ignore();
-	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
 	return 0;
 }
@@ -186,20 +136,6 @@ int main(int argc, char* argv[])
 void get_args(char* argv[], int& input_number)
 {
 	input_number = strtol(argv[1], NULL, 10);
-}
-
-void print1(vector<int> vec)
-{
-	int subvector = 0;
-	for (int i = 0; i < vec.size(); i++)
-	{
-		if (i % range == 0) {
-			cout << endl << endl << "Subvector Number: " << subvector;
-			subvector++;
-			cout << endl << endl << endl;
-		}
-		cout << vec[i] << " ";
-	}
 }
 
 void print2(vector<int> vec)
